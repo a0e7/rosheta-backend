512929d06120941571e3a957b22a5f48c3838ffe
a0e7 <ameer.eshaq2002@gmail.com>
2024-12-16 22:14:40 +0300

    feat

--- controllers/auth/Create-admin.js
+++ controllers/auth/Create-admin.js
@@ -9,7 +9,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 exports.adminCreate = async (req, res, next) => {
-  let adminRole = await Role.findOne({ name: "admin" }); // Changed to let from const
+  let adminRole = await Role.findOne({ name: "admin" });
   if (!adminRole) {
     const newRole = new Role({ name: "admin" });
     await newRole.save();
@@ -18,13 +18,17 @@ exports.adminCreate = async (req, res, next) => {
   const phoneNumber = req.body.phoneNumber;
   const password = req.body.password;
   try {
+    const user = User.findOne({ phoneNumber: phoneNumber });
+    if (phoneNumber === user.phoneNumber) {
+      res.status(400).json({ message: "there is a user with this number" });
+    }
     const hashedpw = await bcrypt.hash(password, 12);
-    const user = new User({
+    const newuser = new User({
       phoneNumber: phoneNumber,
       password: hashedpw,
       role: adminRole,
     });
-    user.save();
+    newuser.save();
   } catch (err) {
     if (!err.statusCode) {
       console.error("admin create error:", err);
