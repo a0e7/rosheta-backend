5f6dbb5ca8672a800da82baf9a07d806aa90885e
a0e7 <ameer.eshaq2002@gmail.com>
2024-12-21 09:45:18 +0300

    feat

--- models/verfication.js
+++ models/verfication.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const verificationSchema = new Schema({
   phoneNumber: { type: Number, required: true },
   verificationCode: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: 300 }, // Document expires after 5 minutes
+  createdAt: { type: Date, default: Date.now, expires: 300 },
 });
 
 // Create the model
